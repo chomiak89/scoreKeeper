ee368b729f3af3968a553a9cd7a6abbc5f0299e7
Kamil <kchomiak@outlook.com>
2018-09-24 22:30:24 -0500

    html structure and js dom selectors

---
+++ script.js
@@ -0,0 +1,10 @@
+let scorePlayer1 = document.querySelector("#scorePlayer1");
+let scorePlayer2 = document.querySelector("#scorePlayer2");
+
+let scoreToWin = document.querySelector(".scoreToWin");
+
+let scoreSelect = document.querySelector(".scoreSelect");
+
+let btnPlayer1 = document.querySelector(".btnPlayer1");
+let btnPlayer2 = document.querySelector(".btnPlayer2");
+let btnReset = document.querySelector(".btnReset");
